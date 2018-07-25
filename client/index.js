a723caf7df5e93a875403866a9f1d25b1ee46bb0
MrHuxu <hxtheone@gmail.com>
2018-07-26 01:57:54 +0800

    make the router at frontend and backend work

--- client/index.js
+++ client/index.js
@@ -4,12 +4,14 @@ import { renderToString } from 'react-dom/server';
 import { Switch, Route, StaticRouter } from 'react-router';
 import routes from './routes';
 
-export const genHtmlString = url => renderToString(
-  <StaticRouter url={ url }>
-    <Switch>
-      { routes.map(route => (
-        <Route { ...route } />
-      )) }
-    </Switch>
-  </StaticRouter>
-);
+export const genHtmlString = url => {
+  return renderToString(
+    <StaticRouter location={ url }>
+      <Switch>
+        { routes.map(route => (
+          <Route { ...route } />
+        )) }
+      </Switch>
+    </StaticRouter>
+  );
+};
--- server/routes.go
+++ server/routes.go
@@ -1,22 +1,38 @@
 package server
 
 import (
+	"fmt"
+
 	"github.com/dop251/goja"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *server) registerRoutes() {
-	s.Engine.GET("/test", func(ctx *gin.Context) {
+	s.Engine.GET("/", func(ctx *gin.Context) {
 		vm := goja.New()
-		_, err := vm.RunString(s.react)
+		v, err := vm.RunString(s.react)
 		if err != nil {
 			panic(err)
 		}
 
-		var fn func(string) string
-		vm.ExportTo(vm.Get("genHtmlString"), &fn)
+		fn := v.Export().(map[string]interface{})["genHtmlString"].(func(goja.FunctionCall) goja.Value)
+		url, _ := vm.RunString(fmt.Sprintf("\"%s\"", ctx.Request.URL.String()))
 
-		ctx.Writer.Write([]byte(fn(ctx.Request.URL.String())))
+		ctx.Writer.Write([]byte(fn(goja.FunctionCall{Arguments: []goja.Value{url}}).String()))
+		return
+	})
+
+	s.Engine.GET("/test", func(ctx *gin.Context) {
+		vm := goja.New()
+		v, err := vm.RunString(s.react)
+		if err != nil {
+			panic(err)
+		}
+
+		fn := v.Export().(map[string]interface{})["genHtmlString"].(func(goja.FunctionCall) goja.Value)
+		url, _ := vm.RunString(fmt.Sprintf("\"%s\"", ctx.Request.URL.String()))
+
+		ctx.Writer.Write([]byte(fn(goja.FunctionCall{Arguments: []goja.Value{url}}).String()))
 		return
 	})
 }
