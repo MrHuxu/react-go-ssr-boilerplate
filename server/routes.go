3c05fefdd18b5c5c14b7d60864de8ff27cd349f7
MrHuxu <hxtheone@gmail.com>
2018-07-26 21:29:01 +0800

    refactor the code of backend

--- server/routes.go
+++ server/routes.go
@@ -1,43 +1,31 @@
 package server
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/dop251/goja"
 	"github.com/gin-gonic/gin"
-
-	"github.com/MrHuxu/react-go-ssr-boilerplate/server/react"
 )
 
 func (s *server) registerRoutes() {
 	s.Engine.GET("/", func(ctx *gin.Context) {
-		url, _ := react.VM.RunString(fmt.Sprintf("'%s'", ctx.Request.URL.String()))
-
-		bytes, _ := json.Marshal(map[string]interface{}{
-			"home": map[string]interface{}{
-				"titles": []string{"test title"},
-				"infos":  map[string]interface{}{"test title": 1},
+		ctx.Writer.Write([]byte(s.renderHTMLString(
+			ctx.Request.URL.String(),
+			map[string]interface{}{
+				"home": map[string]interface{}{
+					"titles": []string{"test title"},
+					"infos":  map[string]interface{}{"test title": 1},
+				},
 			},
-		})
-		data, _ := react.VM.RunString(fmt.Sprintf("'%s'", string(bytes)))
-
-		ctx.Writer.Write([]byte(react.Render(goja.FunctionCall{Arguments: []goja.Value{url, data}}).String()))
-		return
+		)))
 	})
 
 	s.Engine.GET("/test", func(ctx *gin.Context) {
-		url, _ := react.VM.RunString(fmt.Sprintf("'%s'", ctx.Request.URL.String()))
-
-		bytes, _ := json.Marshal(map[string]interface{}{
-			"test": map[string]interface{}{
-				"list":  []string{"test"},
-				"infos": map[string]int{"test": 2},
+		ctx.Writer.Write([]byte(s.renderHTMLString(
+			ctx.Request.URL.String(),
+			map[string]interface{}{
+				"test": map[string]interface{}{
+					"list":  []string{"test"},
+					"infos": map[string]int{"test": 2},
+				},
 			},
-		})
-		data, _ := react.VM.RunString(fmt.Sprintf("'%s'", string(bytes)))
-
-		ctx.Writer.Write([]byte(react.Render(goja.FunctionCall{Arguments: []goja.Value{url, data}}).String()))
-		return
+		)))
 	})
 }
--- server/server.go
+++ server/server.go
@@ -1,12 +1,14 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/dop251/goja"
 	"github.com/gin-gonic/gin"
 
 	"github.com/MrHuxu/react-go-ssr-boilerplate/server/conf"
+	"github.com/MrHuxu/react-go-ssr-boilerplate/server/react"
 )
 
 var DefaultServer Server
@@ -33,4 +35,13 @@ type server struct {
 
 func (s *server) Run() {
 	s.Engine.Run(fmt.Sprintf(":%d", conf.Conf.Web.Port))
+}
+
+func (s *server) renderHTMLString(url string, data interface{}) string {
+	urlVal, _ := react.VM.RunString(fmt.Sprintf("'%s'", url))
+
+	bytes, _ := json.Marshal(data)
+	dataVal, _ := react.VM.RunString(fmt.Sprintf("'%s'", string(bytes)))
+
+	return react.Render(goja.FunctionCall{Arguments: []goja.Value{urlVal, dataVal}}).String()
 }
